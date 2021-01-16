9e4f838a37bc52265928268731f5fbe54fb173f9
geraldbryan <geraldbryan9914@gmail.com>
2021-01-16 19:33:12 +0700

    Change route optimization layout, change size box of booking apps, add profile and app description, add source, change place image name

--- global.R
+++ global.R
@@ -64,23 +64,7 @@ library(leaflet)
     ### Bandung
     
     Bandung_dest <- read.csv("data/Bandung_destination.csv")
-    
-  ## Polylinedecorator plugin for arrows
-    download.file(
-      'https://raw.githubusercontent.com/bbecquet/Leaflet.PolylineDecorator/master/dist/leaflet.polylineDecorator.js',
-      'leaflet.polylineDecorator.js')
-    
-    polylineDecoratorPlugin <- htmlDependency('Leaflet.PolylineDecorator',
-                                              '1.6.0',
-                                              src = normalizePath('.'),
-                                              script = 'leaflet.polylineDecorator.js')
-    
-    registerPlugin <- function(map, plugin) {
-      map$dependencies <- c(map$dependencies, list(plugin))
-      map
-    }
-    
-    
+
 # Booking Application Data
   
   ## Read Data
